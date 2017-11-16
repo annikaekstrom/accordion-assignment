c6b32c8e76fc4db554be5afcc61bb4c498e9e6e0
AnnikaEkstrom <ekstromannika@hotmail.com>
2017-11-16 16:39:49 +0100

    Added slide

--- code/script.js
+++ code/script.js
@@ -1,9 +1,22 @@
 // A function that adds and remove the class "active" on the section you click on.
-function toggle() {
-  this.classList.toggle("active")
-}
+// function toggle() {
+//   this.classList.toggle("active")
+// }
+var acc = document.getElementsByClassName("title");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  }
+}
 // Selects and HTML element, and calls a function which will be executed when the element is clicked.
-document.getElementById("section1").onclick = toggle
-document.getElementById("section2").onclick = toggle
-document.getElementById("section3").onclick = toggle
+// document.getElementById("section1").onclick = toggle
+// document.getElementById("section2").onclick = toggle
+// document.getElementById("section3").onclick = toggle
